b1c7116b61a2222a09bf37d8a00d28b6bab06203
IbraheemxDev <ibraheem.codes@gmail.com>
2026-03-03 21:40:38 +0500

    hello

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
+        <h1 className='text-4xl md:text-5xl font-semibold'>Luxury cars on Rent </h1>
+        <form ></form>
+    </div>
+  )
+}
+
+export default Hero
